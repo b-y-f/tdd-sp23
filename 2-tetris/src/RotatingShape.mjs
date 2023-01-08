ab2782246484ead9fdcb74cba25a1052e7485324
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-08 21:07:01 +1300

    fixed immutable bug

--- 2-tetris/src/RotatingShape.mjs
+++ 2-tetris/src/RotatingShape.mjs
@@ -3,22 +3,25 @@ export class RotatingShape {
     this.shape = shapeStr
       .split("\n")
       .map((s) => s.replace(/\s+/g, "").split(""));
+    Object.freeze(this);
   }
 
   rotateRight() {
     // this function should return a new Shape
-    let newShapeStr = this.shape[0]
-      .map((val, index) => this.shape.map((row) => row[index]).reverse())
+    let copy = this.shape.map((row) => row.slice());
+    let newShapeStr = copy[0]
+      .map((val, index) => copy.map((row) => row[index]).reverse())
       .map((row) => row.join(""))
       .join("\n");
     return new RotatingShape(newShapeStr);
   }
 
   rotateLeft() {
-    let newShapeStr = (this.shape = this.shape[0]
-      .map((val, index) => this.shape.map((row) => row[row.length - 1 - index]))
+    let copy = this.shape.map((row) => row.slice());
+    let newShapeStr = copy[0]
+      .map((val, index) => copy.map((row) => row[row.length - 1 - index]))
       .map((row) => row.join(""))
-      .join("\n"));
+      .join("\n");
     return new RotatingShape(newShapeStr);
   }
 
