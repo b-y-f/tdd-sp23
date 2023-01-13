f4b8519fa67a859566e9c9a558b618c7a7ac9831
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-13 19:38:15 +1300

    pass test: better use + over concat method

--- 2-tetris/src/Board.mjs
+++ 2-tetris/src/Board.mjs
@@ -9,6 +9,13 @@ export class Board {
   }
 
   toString() {
-    return "TODO";
+    let s = "";
+    for (let i = 0; i < this.height; i += 1) {
+      for (let j = 0; j < this.width; j += 1) {
+        s += ".";
+      }
+      s += "\n";
+    }
+    return s;
   }
 }
