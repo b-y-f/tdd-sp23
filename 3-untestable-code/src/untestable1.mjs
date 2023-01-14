c26b7c6a72c9c73beda5ef90c235edc8bf12c4b0
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-14 20:40:24 +1300

    done untestable 1

--- 3-untestable-code/src/untestable1.mjs
+++ 3-untestable-code/src/untestable1.mjs
@@ -1,7 +1,19 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
-export function daysUntilChristmas() {
-  const now = new Date();
+// export function daysUntilChristmas() {
+//   const now = new Date();
+//   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+//   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+//   if (today.getTime() > christmasDay.getTime()) {
+//     christmasDay.setFullYear(new Date().getFullYear() + 1);
+//   }
+//   const diffMillis = christmasDay.getTime() - today.getTime();
+//   return Math.floor(diffMillis / millisPerDay);
+// }
+
+
+// looks like this function can only give days unit xmas for 2023 year.
+export function daysUntilChristmas(now=new Date()) {
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
