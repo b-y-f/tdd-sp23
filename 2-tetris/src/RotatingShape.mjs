4259e97fe5a4eddac4fc84e8a7e94d54338fb382
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-14 13:12:48 +1300

    refactored

--- 2-tetris/src/RotatingShape.mjs
+++ 2-tetris/src/RotatingShape.mjs
@@ -2,15 +2,20 @@ export class RotatingShape {
   #blocks;
 
   constructor(blockString) {
+    this.#initBlocks(blockString);
+    this.width = this.#blocks.length;
+    this.height = this.#blocks[0].length;
+    Object.freeze(this);
+  }
+
+  #initBlocks(blockString) {
     this.#blocks = blockString
       .split("\n")
       .map((s) => s.replace(/\s+/g, "").split(""));
-    this.width = this.#blocks.length;
-    this.height = this.#blocks[0].length;
   }
 
   rotateRight() {
-    const copy = this.#blocks.map((row) => row.slice());
+    const copy = this.#cloneBlocks();
     const newShapeStr = copy[0]
       .map((_, index) => copy.map((row) => row[index]).reverse())
       .map((row) => row.join(""))
@@ -19,12 +24,16 @@ export class RotatingShape {
   }
 
   rotateLeft() {
-    const copy = this.#blocks.map((row) => row.slice());
+    const copy = this.#cloneBlocks();
     const newShapeStr = copy[0]
       .map((_, index) => copy.map((row) => row[row.length - 1 - index]))
       .map((row) => row.join(""))
       .join("\n");
     return new RotatingShape(newShapeStr);
+  }
+
+  #cloneBlocks() {
+    return this.#blocks.map((row) => row.slice());
   }
 
   toString() {
