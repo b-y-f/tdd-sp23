8ad7ebf7a7bd442daa51b18ad84cdc09fc09611f
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-14 22:16:59 +1300

    done untestable 3

--- 3-untestable-code/test/untestable3.test.mjs
+++ 3-untestable-code/test/untestable3.test.mjs
@@ -1,16 +1,34 @@
 import { expect } from "chai";
 import { parsePeopleCsv } from "../src/untestable3.mjs";
+import fs from 'fs';
+
 
 // example input:
 // Loid,Forger,,Male
 // Anya,Forger,6,Female
 // Yor,Forger,27,Female
 
+const rawData = "Loid,Forger,,Male\nAnya,Forger,6,Female\nYor,Forger,27,Female" 
+
+const contents = [{ firstName: 'Loid', lastName: 'Forger', gender: 'm' },
+{ firstName: 'Anya', lastName: 'Forger', gender: 'f', age: 6 },
+{ firstName: 'Yor', lastName: 'Forger', gender: 'f', age: 27 }]
+
+const filename = './people.csv'
+
+beforeEach(() => {
+  fs.writeFileSync(filename, rawData);
+});
+
+afterEach(() => {
+  fs.unlinkSync(filename);
+})
+
 describe("Untestable 3: CSV file parsing", () => {
   it("todo", async () => {
     // TODO: write proper tests
     try {
-      expect(await parsePeopleCsv("people.csv")).to.deep.equal([]);
+      expect(await parsePeopleCsv(filename)).to.deep.equal(contents);
     } catch (e) {}
   });
 });
