023cacf2dbb02b272d8192375dcda79b1052d249
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-15 16:32:45 +1300

    update plan

--- 4-legacy-code/test/gilded_rose.test.mjs
+++ 4-legacy-code/test/gilded_rose.test.mjs
@@ -3,8 +3,9 @@ import { Item, Shop } from "../src/gilded_rose.mjs";
 
 /**
  * Plan: For this excercise with zero initial test, I need to write
- * some tests to make sure the basic function are good to use,
- * then slowly refactor the code and keep all the test pass, in
+ * some tests to make sure the basic function are good to use, to
+ * measure how much test I write, coverage testing tool will be needed.
+ * Then slowly refactor the code and keep all the test pass, in
  * the end, we will have a good shape of the code. Then we can
  * write some test for the new features, and do the red, green
  * light testing.
