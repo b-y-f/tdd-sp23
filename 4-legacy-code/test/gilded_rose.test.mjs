def28b20462bc5a50704327f8bdd12ba81a2c508
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-15 14:49:45 +1300

    add my plan

--- 4-legacy-code/test/gilded_rose.test.mjs
+++ 4-legacy-code/test/gilded_rose.test.mjs
@@ -1,6 +1,14 @@
 import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
+/**
+ * Plan: For this excercise with zero initial test, I need to write 
+ * some tests to make sure the basic function are good to use, 
+ * then slowly refactor the code and keep all the test pass, in
+ * the end, we will have a good shape of the code. Then we can
+ * write some test for the new features, and do the red, green 
+ * light testing.
+ */
 describe("Gilded Rose", () => {
   it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
