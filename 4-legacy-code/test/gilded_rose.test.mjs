1d36caaddef4292e05c96e29463951e4a0513fd1
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-15 16:24:32 +1300

    refactored

--- 4-legacy-code/test/gilded_rose.test.mjs
+++ 4-legacy-code/test/gilded_rose.test.mjs
@@ -2,17 +2,23 @@ import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 /**
- * Plan: For this excercise with zero initial test, I need to write 
- * some tests to make sure the basic function are good to use, 
+ * Plan: For this excercise with zero initial test, I need to write
+ * some tests to make sure the basic function are good to use,
  * then slowly refactor the code and keep all the test pass, in
  * the end, we will have a good shape of the code. Then we can
- * write some test for the new features, and do the red, green 
+ * write some test for the new features, and do the red, green
  * light testing.
  */
 describe("Gilded Rose", () => {
   it("Hello world test!", () => {
-    const gildedRose = new Shop([new Item("foo", 10, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].toString()).to.equal("foo 9 0");
+    expect(updateTest("foo", 10, 0)).to.equal("foo 9 0");
   });
+
 });
+
+function updateTest(name, sellIn, quality) {
+  const gildedRose = new Shop([new Item(name, sellIn, quality)]);
+  const items = gildedRose.updateQuality();
+  return items[0].toString();
+}
+
