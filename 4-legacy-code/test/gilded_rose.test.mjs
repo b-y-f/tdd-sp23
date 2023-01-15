181a84230b5f01c7c46adfd617d0618a7043b85d
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-15 19:41:35 +1300

    modify test into approval test

--- 4-legacy-code/test/gilded_rose.test.mjs
+++ 4-legacy-code/test/gilded_rose.test.mjs
@@ -1,5 +1,11 @@
 import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
+import { verifyAsJSON } from "approvals";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /**
  * Plan: For this excercise with zero initial test, I need to write
@@ -22,24 +28,35 @@ describe("Gilded Rose", () => {
   });
 });
 
-describe("Combination Test to pass the coverage test", () => {
+describe("Approval Tests", () => {
   const names = ["foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"];
   const sellIns = [0, 1, -1];
   const qualities = [0, 50, 48];
+  const paraCombs = cartesian(names, sellIns, qualities);
 
-  it("combinations", () => {
-    for (const name of names) {
-      for (const sellLn of sellIns) {
-        for (const quality of qualities) {
-          expect(updateTest(name, sellLn, quality)).to.be.a("string");
-        }
-      }
-    }
-  });
+  const testResults = paraCombs.map(
+    ([name, sellIn, quality]) => `[${name}, ${sellIn}, ${quality}] => ${updateTest(name, sellIn, quality)}`
+  );
+  verifyAsJSON(__dirname, "Combination.Test", testResults);
 });
 
 function updateTest(name, sellIn, quality) {
   const gildedRose = new Shop([new Item(name, sellIn, quality)]);
   const items = gildedRose.updateQuality();
   return items[0].toString();
+}
+
+function cartesian(...args) {
+  var r = [],
+    max = args.length - 1;
+  function helper(arr, i) {
+    for (var j = 0, l = args[i].length; j < l; j++) {
+      var a = arr.slice(0); // clone arr
+      a.push(args[i][j]);
+      if (i == max) r.push(a);
+      else helper(a, i + 1);
+    }
+  }
+  helper([], 0);
+  return r;
 }
