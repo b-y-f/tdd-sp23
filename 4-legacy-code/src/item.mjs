fdae6af6977b26f9667e3dcb8a4b1e95d7b6e3e7
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-16 20:54:42 +1300

    refactor: create specific subclasses

--- 4-legacy-code/src/item.mjs
+++ 4-legacy-code/src/item.mjs
@@ -9,3 +9,21 @@ export class Item {
     return `${this.name} | ${this.sellIn} | ${this.quality}`;
   }
 }
+
+export class AgedBrie extends Item {
+  constructor(sellIn, quality) {
+    super("Aged Brie", sellIn, quality);
+  }
+}
+
+export class BackstagePass extends Item {
+  constructor(sellIn, quality) {
+    super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+  }
+}
+
+export class Sulfuras extends Item {
+  constructor(sellIn, quality) {
+    super("Sulfuras, Hand of Ragnaros", sellIn, quality);
+  }
+}
