4cf4c295dd83a4712c98ef6d4df7fbd578724dce
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-05 12:53:28 +1300

    AddTest:more(3) neighbors test

--- 6-game-of-life/tests/World.test.ts
+++ 6-game-of-life/tests/World.test.ts
@@ -16,11 +16,40 @@ describe("Test world, cell and neighbor cells", () => {
 
   it("test rules alive neighbors number", () => {
     const cell: Cell = {
-      col: 2,
       row: 2,
+      col: 2,
       isAlive: true,
     };
     const aliveNeighbors = world.getAliveNeighbors(cell);
     expect(aliveNeighbors).toBe(0);
+  });
+
+  it("more(3) neighbors test", () => {
+    const cell0: Cell = {
+      row: 3,
+      col: 3,
+      isAlive: true,
+    };
+    const cell1: Cell = {
+      row: 4,
+      col: 3,
+      isAlive: true,
+    };
+    const cell2: Cell = {
+      col: 2,
+      row: 2,
+      isAlive: true,
+    };
+    const cell3: Cell = {
+      col: 4,
+      row: 4,
+      isAlive: true,
+    };
+    world.addCell(cell0);
+    world.addCell(cell1);
+    world.addCell(cell2);
+    world.addCell(cell3);
+    const aliveNeighbors = world.getAliveNeighbors(cell0);
+    expect(aliveNeighbors).toBe(3);
   });
 });
