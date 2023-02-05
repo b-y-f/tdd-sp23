549c51bfa6f4921f3ade90546ff01fa805a0d62e
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-05 22:26:18 +1300

    AddTest: More complexed pattern, 47 live cells

--- 6-game-of-life/tests/GameOfLife.test.ts
+++ 6-game-of-life/tests/GameOfLife.test.ts
@@ -23,4 +23,18 @@ x = 2, y = 2, rule = B3/S23
     game.fromRLE(blockString);
     expect(game.getWorld().getNumOfAliveCell()).toBe(4);
   });
+
+  it("More complexed pattern, 48 live cells", () => {
+    const patString = `#N Pulsar
+#O John Conway
+#C A period 3 oscillator. Despite its size, this is the fourth most common oscillator (and by
+#C far the most common of period greater than 2).
+#C www.conwaylife.com/wiki/index.php?title=Pulsar
+x = 13, y = 13, rule = B3/S23
+2b3o3b3o2b2$o4bobo4bo$o4bobo4bo$o4bobo4bo$2b3o3b3o2b2$2b3o3b3o2b$o4bob
+o4bo$o4bobo4bo$o4bobo4bo2$2b3o3b3o!`;
+
+    game.fromRLE(patString);
+    expect(game.getWorld().getNumOfAliveCell()).toBe(48);
+  });
 });
