8b6bd0b9bbe1134b8576819bc636454d242e5e3c
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-07 20:51:02 +1300

    Pass:blom number test

--- 6-game-of-life/tests/Methuselahs.test.ts
+++ 6-game-of-life/tests/Methuselahs.test.ts
@@ -4,17 +4,23 @@ describe("Blom, the cell will grow when more iter", () => {
   let game;
   beforeEach(() => {
     game = new GameOfLife();
-  });
-  it("test number of alive cells after iter", () => {
     const pattern = `x = 12, y = 5, rule = B3/S23
 o10bo$b4o6bo$2b2o7bo$10bo$8bobo!`;
     game.fromRLE(pattern);
+  });
+  it("test number of alive cells after iter:176", () => {
     expect(game.getWorld().getNumOfAliveCell()).toBe(13);
-
     game.iterEvolve(176);
     expect(game.getWorld().getNumOfAliveCell()).toBe(97);
+  });
 
-    game.iterEvolve(596);
-    expect(game.getWorld().getNumOfAliveCell()).toBe(412);
+  it("T=352, alive cell=113", () => {
+    game.iterEvolve(352);
+    expect(game.getWorld().getNumOfAliveCell()).toBe(113);
+  });
+
+  it("T=515, alive cell=324", () => {
+    game.iterEvolve(515);
+    expect(game.getWorld().getNumOfAliveCell()).toBe(324);
   });
 });
