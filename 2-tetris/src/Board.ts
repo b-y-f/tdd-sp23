b678370d4a04388979d7b66126eeca3f25068260
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-15 23:39:48 +1300

    Pass:The board starts empty

--- 2-tetris/src/Board.ts
+++ 2-tetris/src/Board.ts
@@ -1,3 +1,27 @@
 export class Board {
-  constructor() {}
+  private width: number;
+  private height: number;
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    const board: number[][] = [];
+
+    for (let i = 0; i < height; i++) {
+      board[i] = [];
+      for (let j = 0; j < width; j++) {
+        board[i][j] = 0;
+      }
+    }
+  }
+
+  public toString(): string {
+    let s = "";
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        s += ".";
+      }
+      s += "\n";
+    }
+    return s;
+  }
 }
