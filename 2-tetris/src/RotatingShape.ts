1c61250abb6982aedd3b8dedf66df7393c9cd2e9
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-19 18:12:08 +1300

    Pass:can be rotated right/clockwise

--- 2-tetris/src/RotatingShape.ts
+++ 2-tetris/src/RotatingShape.ts
@@ -1,18 +1,46 @@
 export class RotatingShape {
-  private shape: string[][];
-  private readonly height: number;
   private readonly width: number;
+  private readonly height: number;
+  private readonly shape: string[];
+  private orientation: number;
 
   constructor(shape: string) {
-    this.shape = shape
-      .trim()
-      .split(/\s+/)
-      .map((row) => row.split(""));
-    this.height = this.shape.length;
-    this.width = this.shape[0].length;
+    const rows = shape.trim().split("\n");
+    this.height = rows.length;
+    this.width = rows[0].length;
+    this.shape = rows.map((row) => row.trim().padEnd(this.width, "."));
+    this.orientation = 0;
+  }
+
+  public rotateRight(): this {
+    const newShape: string[] = [];
+    for (let col = 0; col < this.width; col++) {
+      let newRow = "";
+      for (let row = this.height - 1; row >= 0; row--) {
+        newRow += this.shape[row][col];
+      }
+      newShape.push(newRow);
+    }
+    this.shape.splice(0, this.height, ...newShape);
+    this.orientation = (this.orientation + 1) % 4;
+    return this;
+  }
+
+  public rotateLeft(): this {
+    const newShape: string[] = [];
+    for (let col = this.width - 1; col >= 0; col--) {
+      let newRow = "";
+      for (let row = 0; row < this.height; row++) {
+        newRow += this.shape[row][col];
+      }
+      newShape.push(newRow);
+    }
+    this.shape.splice(0, this.height, ...newShape);
+    this.orientation = (this.orientation + 3) % 4;
+    return this;
   }
 
   public toString(): string {
-    return this.shape.map((row) => row.join("")).join("\n") + "\n";
+    return this.shape.join("\n") + "\n";
   }
 }
