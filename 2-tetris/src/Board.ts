7b60cc0c46a8b60035afffd4e0c915d0abd27314
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-19 17:19:08 +1300

    PassTest:When a block lands on another block

--- 2-tetris/src/Board.ts
+++ 2-tetris/src/Board.ts
@@ -3,21 +3,23 @@ import { Block } from "./Block";
 export class Board {
   private width: number;
   private height: number;
-  private board: number[][];
+  private board: (string | undefined)[][];
   private currentBlock: Block | undefined;
+  private fixed: Block[];
   constructor(width: number, height: number) {
     this.currentBlock = undefined;
+    this.fixed = [];
     this.width = width;
     this.height = height;
     this.board = this.createBoard(height, width);
   }
 
-  private createBoard(height: number, width: number): number[][] {
-    const board: number[][] = [];
+  private createBoard(height: number, width: number): string[][] {
+    const board: string[][] = [];
     for (let i = 0; i < height; i++) {
       board[i] = [];
       for (let j = 0; j < width; j++) {
-        board[i][j] = 0;
+        board[i][j] = ".";
       }
     }
     return board;
@@ -29,12 +31,14 @@ export class Board {
 
   public tick(): void {
     if (this.currentBlock?.getY() === this.height - 1) {
+      this.fixed.push(this.currentBlock);
       this.currentBlock = undefined;
     } else {
       this.currentBlock?.moveDown();
       this.updateBoard();
     }
   }
+
   public drop(blk: Block): void {
     if (this.hasFalling()) {
       throw new Error("already falling");
@@ -51,9 +55,17 @@ export class Board {
           row === this.currentBlock?.getY() &&
           col === this.currentBlock?.getX()
         ) {
-          this.board[row][col] = 1;
+          this.board[row][col] = this.currentBlock?.getColor();
         } else {
-          this.board[row][col] = 0;
+          this.board[row][col] = ".";
+        }
+
+        const foundBlock = this.fixed.find(
+          (blk) => blk.getY() === row && blk.getX() === col
+        );
+        if (foundBlock) {
+          this.board[foundBlock.getY()][foundBlock.getX()] =
+            foundBlock.getColor();
         }
       }
     }
@@ -63,11 +75,7 @@ export class Board {
     let s = "";
     for (let row = 0; row < this.height; row++) {
       for (let col = 0; col < this.width; col++) {
-        if (this.board[row][col] === 0) {
-          s += ".";
-        } else {
-          s += "X";
-        }
+        s += this.board[row][col];
       }
       s += "\n";
     }
--- 2-tetris/test/FallingBlocks.test.ts
+++ 2-tetris/test/FallingBlocks.test.ts
@@ -81,37 +81,39 @@ describe("Falling blocks", () => {
     });
   });
 
-  //   describe("When a block lands on another block", () => {
-  //     beforeEach(() => {
-  //       board.drop(new Block("X"));
-  //       board.tick();
-  //       board.tick();
-  //       board.tick();
-  //       board.drop(new Block("Y"));
-  //       board.tick();
-  //     });
+  describe("When a block lands on another block", () => {
+    beforeEach(() => {
+      board.drop(new Block("X"));
+      board.tick();
+      board.tick();
+      board.tick();
+      board.drop(new Block("Y"));
+      board.tick();
+    });
 
-  //     it("it is still moving on the row above the other block", () => {
-  //       expect(board.toString()).to.equalShape(
-  //         `...
-  //        .Y.
-  //        .X.`
-  //       );
-  //       expect(
-  //         board.hasFalling(),
-  //         "the player should still be able to move the block"
-  //       ).to.be.true;
-  //     });
+    it("it is still moving on the row above the other block", () => {
+      // @ts-ignore
+      expect(board.toString()).to.equalShape(
+        `...
+         .Y.
+         .X.`
+      );
+      expect(
+        board.hasFalling(),
+        "the player should still be able to move the block"
+      ).to.be.true;
+    });
 
-  //     it("it stops when it hits the other block", () => {
-  //       board.tick();
+    xit("it stops when it hits the other block", () => {
+      board.tick();
 
-  //       expect(board.toString()).to.equalShape(
-  //         `...
-  //        .Y.
-  //        .X.`
-  //       );
-  //       expect(board.hasFalling(), "the block should stop moving").to.be.false;
-  //     });
-  //   });
+      // @ts-ignore
+      expect(board.toString()).to.equalShape(
+        `...
+         .Y.
+         .X.`
+      );
+      expect(board.hasFalling(), "the block should stop moving").to.be.false;
+    });
+  });
 });
